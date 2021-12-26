7686919d452438e01e86e1994404576765ae2e59
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-26 16:23:45 +0330

    search  box added

---
+++ components/TheSearchBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="searchbox flex ">
+      <input class="flex-1" placeholder="Search nft ,Character, ..." type="text"><button class="justify-center items-center flex">
+          <img src="../assets/images/search.svg" alt="">
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
