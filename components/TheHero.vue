b43e5543babaec3760e5efcb41f828c5d00b9e14
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-26 13:08:41 +0330

    hero nav completed

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="hero">
-    
+    <nav class="flex">
+      <div class="logo"></div>
+      <div class="links flex justify-between text-white">
+        <p class="cursor-pointer">Home</p>
+        <p class="cursor-pointer">Nft Shop</p>
+        <p class="cursor-pointer">About</p>
+        <p class="cursor-pointer">Contact Us</p>
+      </div>
+      <div class="buttons flex justify-between">
+        <div class="h-full cart cursor-pointer"></div>
+        <div class="divider"></div>
+        <div class="profile h-full w-6"></div>
+      </div>
+    </nav>
   </div>
 </template>
 
