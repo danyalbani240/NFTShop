b94482f88167e76f4249cdf08c796ef4dc131b7d
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-26 17:17:47 +0330

    the sections added

---
+++ components/TheSections.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sections flex justify-between items-center mt-4">
+      <div class="flex flex-col items-center">
+          <img src="../assets/images/film.svg" alt="">
+          <p class="mt-2.5">Film</p>
+      </div>
+      <div class="flex flex-col items-center">
+          <img src="../assets/images/music.svg" alt="">
+          <p class="mt-2.5">Music</p>
+      </div>
+      <div class="flex flex-col items-center">
+          <img src="../assets/images/picture.svg" alt="">
+          <p class="mt-2.5">Picture</p>
+      </div>
+      <div class="flex flex-col items-center">
+          <img src="../assets/images/animation.svg" alt="">
+          <p class="mt-2.5">Animation</p>
+      </div>
+      <div class="flex flex-col items-center">
+          <img src="../assets/images/paint.svg" alt="">
+          <p class="mt-2.5">Painting</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
