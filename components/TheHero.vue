cac83aad313c10234c14f42c6cddb83275376615
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-26 16:23:36 +0330

    hero is done

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -14,11 +14,22 @@
         <div class="profile h-full w-6"></div>
       </div>
     </nav>
+    <div class="hero-comment mt-32">
+      <h2 class="text-4xl font-bold">Turn Clicks Into</h2>
+      <img  src="../assets/images/NFText.svg" alt="">
+      <p >Must explain to you how all this mistaken idea of pleasure and praising pain was born and will give you a complete account of the system, and expound the actual teachings.</p>
+      <div class="register text-white flex justify-center items-center">
+        Register
+      </div>
+    </div>
+    <the-search-box />
   </div>
 </template>
 
 <script>
+import TheSearchBox from './TheSearchBox.vue'
 export default {
+  components: { TheSearchBox },
 
 }
 </script>
