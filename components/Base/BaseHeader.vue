162aa4bd8f1585dae2142318f2524976c76bf342
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-28 12:11:08 +0330

    Base HEader created

---
+++ components/Base/BaseHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex">
+      <p>
+          <slot> </slot>
+      </p>
+      <div class="flex-1 lev"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+p{
+    color: rgba(0, 0, 0,0.7);
+    font-size: 24px;
+    margin-right: 36px;
+}
+.lev{
+    height: 1px;
+    background-color: rgba(0, 0, 0, 0.16);
+}
+</style>
