89f56c9481356ff6390a644af7b9093bb2d5687c
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-28 10:32:08 +0330

    Base Card Component added

---
+++ components/Base/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+      <div class="card-image"></div>
+      <div class="card-body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
