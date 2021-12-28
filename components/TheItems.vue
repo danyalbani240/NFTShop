da573188da91d104a22407ab0337f6b6e119597b
DanyalBani <mr.d.bani.d@gmail.com>
2021-12-28 10:33:44 +0330

    items Compoentnt added

---
+++ components/TheItems.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="items container flex flex-wrap justify-between">
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
