b10939d30e74735cba8776cc09fe2c69deb982ae
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-02 00:38:51 +0330

    Base Upload Created

---
+++ components/Base/BaseUpload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="upload flex flex-col items-center justify-center">
+    <img src="../../assets/images/upload.svg" alt="" />
+    <p>upload</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.upload {
+  border: 1px solid #c91efa;
+  box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  p {
+    color: #c91efa52;
+  }
+}
+</style>
