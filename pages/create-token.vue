87a82c8aaecb5678f1809b2b54d69dfbb4201970
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-02 12:11:06 +0330

    Create Token Page Created

---
+++ pages/create-token.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="background-color: #f3f3f3">
+    <TheHeader />
+    <ThePhilipeEagle />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
