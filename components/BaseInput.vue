0a73acfa15a987923d4482949ebf2b128b6ecf3d
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-02 12:09:58 +0330

    Base Inpput Created

---
+++ components/BaseInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex base-input-container justify-between items-center">
+    <p>
+      <slot></slot>
+    </p>
+    <input type="text" :placeholder="placeholder" class="base-input h-full w-563" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.base-input-container {
+  background-color: rgba(244, 244, 244, 0.97);
+  min-height: 50px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.32);
+  border-radius: 12px;
+  overflow-x: hidden;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.32);
+  p {
+    color: #1d1355;
+    font-size: 18px;
+    margin-left: 36px;
+  }
+  input {
+    border-radius: 22.5px 12px 12px 22.5px;
+    border: 1px solid #c91efa;
+    min-height: 50px;
+    padding-left: 24px;
+  }
+  input::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+    font-size: 14px;
+  }
+  .w-563 {
+    width: 563px;
+  }
+}
+</style>
