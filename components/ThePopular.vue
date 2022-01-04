da948e641194450500673e55d817a5e5e0a3ddc2
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-04 11:31:54 +0330

    animation added to popular items

--- components/ThePopular.vue
+++ components/ThePopular.vue
@@ -4,10 +4,10 @@
         <img src="../assets/images/prev.svg" alt="">
       </div>
       <div class="popular-items-container flex flex-wrap justify-between">
-          <BaseCard />
-          <BaseCard />
-          <BaseCard />
-          <BaseCard />
+          <BaseCard data-aos="flip-left" data-aos-duration="500" />
+          <BaseCard data-aos="flip-left" data-aos-duration="800" />
+          <BaseCard data-aos="flip-left" data-aos-duration="1000" />
+          <BaseCard data-aos="flip-left" data-aos-duration="1300" />
       </div>
       <div class="next cursor-pointer flex items-center justify-center">
         <img src="../assets/images/next.svg" alt="">
