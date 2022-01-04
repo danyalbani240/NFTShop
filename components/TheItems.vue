18df2cb808093ef265cc8824dd19acae018e794c
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-04 11:26:07 +0330

    duration added to items animation

--- components/TheItems.vue
+++ components/TheItems.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="items container flex flex-wrap justify-between mx-auto" data-aos="fade-down">
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
-      <BaseCard data-aos="fade-down" />
+      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
+      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
+      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
+      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
   </div>
 </template>
 
