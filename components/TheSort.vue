ec96b4c62a4f3c5f8c4ed5853fa80ce4d8741a14
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-10 23:48:12 +0330

    sort 360px media added

--- components/TheSort.vue
+++ components/TheSort.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="sort-container flex items-center justify-between mx-auto">
+  <div class="sort-container flex items-center justify-between mx-auto text-sm">
       <div class="flex text-white cursor-pointer">
-          <img class="mr-4" src="../assets/images/sortburger.svg" alt="">
+          <img class="sm:mr-4 mr-1" src="../assets/images/sortburger.svg" alt="">
           <span>BestSelling</span>
       </div>
       <div class="flex text-white cursor-pointer">
-          <img class="mr-4" src="../assets/images/Alphabetical.svg" alt="">
+          <img class="sm:mr-4 mr-1" src="../assets/images/Alphabetical.svg" alt="">
           <span>Alphabetical</span>
       </div>
       <div class="flex text-white cursor-pointer">
-          <img class="mr-4" src="../assets/images/timesort.svg" alt="">
+          <img class="sm:mr-4 mr-1" src="../assets/images/timesort.svg" alt="">
           <span>Time</span>
       </div>
   </div>
