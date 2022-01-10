c5da385846aa74bae9addf44fd3f96a9612cfc01
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-11 02:05:24 +0330

    footer works in 360 right now

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="footer w-full flex justify-between text-white">
+  <div class="footer w-full flex-wrap flex justify-between text-white">
     <div class="text-4xl mt-12 ml-16">logo name</div>
-    <div class="footer-secs">
-      <p class="text-2xl">About Us</p>
-      <p>Abbout NFTShop</p>
-      <p>Comunity</p>
-      <p>NFT Blog</p>
-      <p>Terms</p>
-    </div>
-    <div class="footer-secs">
-      <p class="text-2xl">Products</p>
-      <p>Exchange</p>
-      <p>Nft Charity Shop</p>
-      <p>Academy</p>
-    </div>
-    <div class="footer-secs">
-      <p class="text-2xl">Service</p>
-      <p>Buy Crypto</p>
-    </div>
-    <div class="footer-secs">
-      <p class="text-2xl">Support</p>
-      <p>FAQ</p>
-      <p>Help Center</p>
-      <p>Give Us Feedback</p>
+    <div class="flex ml-16 justify-center flex-wrap">
+      <div class="footer-secs">
+        <p class="text-2xl">About Us</p>
+        <p>Abbout NFTShop</p>
+        <p>Comunity</p>
+        <p>NFT Blog</p>
+        <p>Terms</p>
+      </div>
+      <div class="footer-secs">
+        <p class="text-2xl">Products</p>
+        <p>Exchange</p>
+        <p>Nft Charity Shop</p>
+        <p>Academy</p>
+      </div>
+      <div class="footer-secs">
+        <p class="text-2xl">Service</p>
+        <p>Buy Crypto</p>
+      </div>
+      <div class="footer-secs">
+        <p class="text-2xl">Support</p>
+        <p>FAQ</p>
+        <p>Help Center</p>
+        <p>Give Us Feedback</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
