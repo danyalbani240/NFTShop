2bdb96eb887995a82d2466e6e7faa1ba3fa27c48
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-11 21:05:16 +0330

    footer is ok

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="footer w-full flex-wrap flex justify-between text-white">
+  <div class="footer w-full flex-col  flex  text-white">
     <div class="text-4xl mt-12 ml-16">logo name</div>
-    <div class="flex ml-16 justify-center flex-wrap">
-      <div class="footer-secs">
+    <div class="flex ml-16 justify-center sm:justify-evenly mt-10 flex-wrap">
+      <div class="footer-secs flex-1">
         <p class="text-2xl">About Us</p>
         <p>Abbout NFTShop</p>
         <p>Comunity</p>
         <p>NFT Blog</p>
         <p>Terms</p>
       </div>
-      <div class="footer-secs">
+      <div class="footer-secs flex-1">
         <p class="text-2xl">Products</p>
         <p>Exchange</p>
         <p>Nft Charity Shop</p>
         <p>Academy</p>
       </div>
-      <div class="footer-secs">
+      <div class="footer-secs flex-1">
         <p class="text-2xl">Service</p>
         <p>Buy Crypto</p>
       </div>
-      <div class="footer-secs">
+      <div class="footer-secs flex-1">
         <p class="text-2xl">Support</p>
         <p>FAQ</p>
         <p>Help Center</p>
