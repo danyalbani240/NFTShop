2871835187b54cdd5adc21dae2558d960c02cc5a
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-24 18:25:03 +0330

    shadow hover is ok

--- components/Base/BaseUpload.vue
+++ components/Base/BaseUpload.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="upload flex flex-col items-center justify-center">
+  <div
+    @click="$emit('click', $event)"
+    class="upload flex flex-col items-center cursor-pointer justify-center transition-all"
+  >
     <img src="../../assets/images/upload.svg" alt="" />
     <p>upload</p>
   </div>
@@ -12,12 +15,15 @@ export default {}
 <style scoped lang="scss">
 .upload {
   border: 1px solid #c91efa;
-  box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.1);
+  box-shadow: none;
   border-radius: 10px;
   width: 128px;
   height: 117px;
   p {
     color: #c91efa52;
   }
+}
+.upload:hover {
+  box-shadow: 0px 0px 9px 1px rgba(0, 0, 0, 0.1);
 }
 </style>
