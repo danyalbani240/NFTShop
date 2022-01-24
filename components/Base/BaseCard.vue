245cce1687a9e8891faf08b2c24251fabb32a2b4
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-25 02:39:39 +0330

    maked as a link to product page

--- components/Base/BaseCard.vue
+++ components/Base/BaseCard.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="card">
+  <nuxt-link to="1"
+    ><div class="card">
       <div class="card-image bg-cover"></div>
       <div class="card-body pt-2.5">
         <p class="text-sm ml-3">Colourful ASA</p>
         <p class="text-xs mr-5 text-right mt-1">179.00BUSD</p>
-        <p style="font-size:10px;" class="mr-5 text-right">≈= $ 1,999.00</p>
-        <div style="font-size:10px;" class="creator flex ml-4 items-center mt-1 ">
-          <img src="../../assets/images/creator.svg" alt="">
-          <p class="ml-1">Creator: <span class="inline-block ml-1">Colourful ASA</span></p>
+        <p style="font-size: 10px" class="mr-5 text-right">≈= $ 1,999.00</p>
+        <div
+          style="font-size: 10px"
+          class="creator flex ml-4 items-center mt-1"
+        >
+          <img src="../../assets/images/creator.svg" alt="" />
+          <p class="ml-1">
+            Creator: <span class="inline-block ml-1">Colourful ASA</span>
+          </p>
         </div>
       </div>
-  </div>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
