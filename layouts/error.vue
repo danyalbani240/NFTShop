84aba301d322ebb3e1c4034d04adf1da81357823
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-24 18:11:08 +0330

    error layout or same the not found page added

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="not-found">
+    <TheHero />
+    <TheSections />
+    <TheSort />
+    <nuxt-link to="/">
+      <div class="not-found flex justify-center flex-col">
+        <img
+          class="max-w-2xl mx-auto mt-32"
+          src="../assets/images/not-found.svg"
+          alt=""
+        />
+        <p class="mt-14 text-8xl text-center">Not Found</p>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
