b18eb537f33dda01c04d45a95a9019113b28ce7e
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-26 01:48:50 +0330

    footer centerlized / items are bigger with now

--- components/TheItems.vue
+++ components/TheItems.vue
@@ -1,26 +1,73 @@
 <template>
-  <div class="items container flex flex-wrap justify-center sm:justify-between mx-auto" data-aos="fade-down">
-      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
-      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
-      <BaseCard data-aos="fade-down" data-aos-duration="1500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2000" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2500" />
-      <BaseCard data-aos="fade-down" data-aos-duration="2700" />
+  <div
+    class="items container flex flex-wrap justify-center sm:justify-between mx-auto"
+    data-aos="fade-down"
+  >
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="1500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2700"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="1500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2700"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="1500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2500"
+    />
+    <BaseCard
+      class="2xl:w-3/12"
+      data-aos="fade-down"
+      data-aos-duration="2700"
+    />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
