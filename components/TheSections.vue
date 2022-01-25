e602b39ae5076484b4301c8e61ede58db79bb59c
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-25 23:16:14 +0330

    sections background added

--- components/TheSections.vue
+++ components/TheSections.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="sections flex justify-between mx-auto items-center mt-4">
-      <div class="flex flex-col items-center ">
-          <img class="transition-all" src="../assets/images/film.svg" alt="">
-          <p class="mt-2.5">Film</p>
+  <div class="sections-container">
+    <div class="sections flex justify-between mx-auto items-center mt-4">
+      <div class="flex flex-col items-center">
+        <img class="transition-all" src="../assets/images/film.svg" alt="" />
+        <p class="mt-2.5">Film</p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img class="transition-all" src="../assets/images/music.svg" alt="">
-          <p class="mt-2.5">Music</p>
+      <div class="flex flex-col items-center">
+        <img class="transition-all" src="../assets/images/music.svg" alt="" />
+        <p class="mt-2.5">Music</p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img class="transition-all" src="../assets/images/picture.svg" alt="">
-          <p class="mt-2.5">Picture</p>
+      <div class="flex flex-col items-center">
+        <img class="transition-all" src="../assets/images/picture.svg" alt="" />
+        <p class="mt-2.5">Picture</p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img class="transition-all" src="../assets/images/animation.svg" alt="">
-          <p class="mt-2.5">Animation</p>
+      <div class="flex flex-col items-center">
+        <img
+          class="transition-all"
+          src="../assets/images/animation.svg"
+          alt=""
+        />
+        <p class="mt-2.5">Animation</p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img class="transition-all" src="../assets/images/paint.svg" alt="">
-          <p class="mt-2.5">Painting</p>
+      <div class="flex flex-col items-center">
+        <img class="transition-all" src="../assets/images/paint.svg" alt="" />
+        <p class="mt-2.5">Painting</p>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
