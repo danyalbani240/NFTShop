bc52040f44e730da71afeedb6dc045321556479e
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-26 02:05:33 +0330

    making the sections slider

--- components/TheSections.vue
+++ components/TheSections.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sections-container">
-    <div class="sections flex justify-between mx-auto items-center mt-4">
+    <div
+      class="sections md:flex hidden justify-between mx-auto items-center mt-4"
+    >
       <div class="flex flex-col items-center">
         <img class="transition-all" src="../assets/images/film.svg" alt="" />
         <p class="mt-2.5">Film</p>
@@ -26,11 +28,73 @@
         <p class="mt-2.5">Painting</p>
       </div>
     </div>
+    <div v-swiper:mySwiper="options">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="flex flex-col items-center">
+            <img
+              class="transition-all"
+              src="../assets/images/film.svg"
+              alt=""
+            />
+            <p class="mt-2.5">Film</p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="flex flex-col items-center">
+            <img
+              class="transition-all"
+              src="../assets/images/music.svg"
+              alt=""
+            />
+            <p class="mt-2.5">Music</p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="flex flex-col items-center">
+            <img
+              class="transition-all"
+              src="../assets/images/picture.svg"
+              alt=""
+            />
+            <p class="mt-2.5">Picture</p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="flex flex-col items-center">
+            <img
+              class="transition-all"
+              src="../assets/images/animation.svg"
+              alt=""
+            />
+            <p class="mt-2.5">Animation</p>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="flex flex-col items-center">
+            <img
+              class="transition-all"
+              src="../assets/images/paint.svg"
+              alt=""
+            />
+            <p class="mt-2.5">Painting</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      options: {
+        slidesPerView: 3,
+      },
+    }
+  },
+}
 </script>
 
 <style></style>
