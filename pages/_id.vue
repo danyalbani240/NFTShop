3804f4775a0d7007f427abf4e6c5bf980ca72aa8
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-26 04:41:21 +0330

    icons added

--- pages/_id.vue
+++ pages/_id.vue
@@ -2,14 +2,29 @@
   <div style="background-color: #f3f3f3">
     <TheHeader />
     <div class="pl-5 w-full flex items-center navigation">
-      <p>Home</p>
-      <p>Shop</p>
-      <p>Picture</p>
+      <p class="flex items-center">
+        Home
+        <img
+          class="ml-1.5 mr-1 flex-1"
+          src="../assets/images/navifationArrow.svg"
+          alt=""
+        />
+      </p>
+      <p class="flex items-center">
+        Shop
+        <img
+          class="ml-1.5 mr-1 flex-1"
+          src="../assets/images/navifationArrow.svg"
+          alt=""
+        />
+      </p>
+      <p style="color: #1d1355">Picture</p>
     </div>
     <TheDetail />
     <TheRelated />
-    <div class="flex justify-between flex-wrap " style="padding: 67px 30px">
-      <img class="flex-1" src="../assets/images/52.svg" alt="" /><img class="flex-1"
+    <div class="flex justify-between flex-wrap" style="padding: 67px 30px">
+      <img class="flex-1 m-2.5" src="../assets/images/52.svg" alt="" /><img
+        class="flex-1 m-2.5"
         src="../assets/images/52.svg"
         alt=""
       />
@@ -21,8 +36,7 @@
 
 <script>
 export default {
-  validate({params,query}) {
-    
+  validate({ params, query }) {
     return params.id == 1
   },
 }
