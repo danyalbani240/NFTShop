f5e041288cb8ab8416f55adc028306b56ab37397
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-28 19:44:13 +0330

    making the benefits in the same grid line with popular

--- components/TheBenefits.vue
+++ components/TheBenefits.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="flex benefits justify-around mx-auto flex-wrap">
-      <div class="flex flex-col items-center ">
-          <img src="../assets/images/bro.svg" alt="">
-          <div class="text-center">
-              <p class="title">Buy the effect</p>
-              <p class="description mx-auto">
-                  Get nft from your owner
-              </p>
-          </div>
+  <div
+    class="flex container benefits justify-center sm:justify-between mx-auto flex-wrap"
+  >
+    <div class="flex flex-col items-center">
+      <img class="b-object" src="../assets/images/bro.svg" alt="" />
+      <div class="text-center">
+        <p class="title">Buy the effect</p>
+        <p class="description mx-auto">Get nft from your owner</p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img src="../assets/images/safe.svg" alt="">
-          <div class="text-center">
-              <p class="title">Security</p>
-              <p class="description mx-auto">You can buy and enjoy in complete security and use all the facilities</p>
-          </div>
+    </div>
+    <div class="flex flex-col my-14 sm:my-0 items-center">
+      <img class="b-object" src="../assets/images/safe.svg" alt="" />
+      <div class="text-center">
+        <p class="title">Security</p>
+        <p class="description mx-auto">
+          You can buy and enjoy in complete security and use all the facilities
+        </p>
       </div>
-      <div class="flex flex-col items-center ">
-          <img src="../assets/images/delivery.svg" alt="">
-          <div class="text-center">
-              <p class="title">fast sending</p>
-              <p class="description mx-auto">Fast shipping worldwide with the lowest cost</p>
-          </div>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="b-object" src="../assets/images/delivery.svg" alt="" />
+      <div class="text-center mt-2">
+        <p class="title">fast sending</p>
+        <p class="description mx-auto">
+          Fast shipping worldwide with the lowest cost
+        </p>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
