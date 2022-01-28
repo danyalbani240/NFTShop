6e312e77c700a3ca32d365c91cb5abd02bd458b9
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-28 19:44:44 +0330

    icons resized and transition addedto them

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -22,34 +22,49 @@
         <p class="cursor-pointer">Contact Us</p>
       </div>
       <div class="buttons hidden lg:flex justify-between">
-        <div class="h-full cart cursor-pointer"></div>
+        <div class="h-full cart transition-all cursor-pointer">
+          <img src="../assets/images/cart.svg" alt="" />
+        </div>
         <div class="divider"></div>
-        <div class="profile cursor-pointer h-full w-6"></div>
+        <div class="profile cursor-pointer transition-all h-full">
+          <img src="../assets/images/person.svg" alt="" />
+        </div>
       </div>
     </nav>
-    <div
-      class="hero-comment ml-2 inline-flex flex-col items-center justify-center mt-10 sm:mt-14 2xl:mt-44 md:mt-20 lg:mt-28"
-    >
-      <h2 class="sm:text-2xl lg:text-4xl text-lg font-bold">
-        Turn Clicks Into
-      </h2>
-      <img class="nft-head" src="../assets/images/NFText.svg" alt="" />
-      <p class="hidden sm:block">
-        Must explain to you how all this mistaken idea of pleasure and praising
-        pain was born and will give you a complete account of the system, and
-        expound the actual teachings.
-      </p>
+    <div class="flex justify-between mt-8 sm:mt-14 2xl:mt-40 md:mt-20 lg:mt-28">
       <div
-        class="register mt-5 ml-0 sm:mx-auto text-white flex justify-center items-center"
+        class="hero-comment ml-2 inline-flex flex-col items-center justify-center"
       >
-        Register
+        <h2 class="sm:text-2xl lg:text-4xl text-lg font-bold">
+          Turn Clicks Into
+        </h2>
+        <img class="nft-head" src="../assets/images/NFText.svg" alt="" />
+        <p class="hidden sm:block">
+          Must explain to you how all this mistaken idea of pleasure and
+          praising pain was born and will give you a complete account of the
+          system, and expound the actual teachings.
+        </p>
+        <div
+          class="register cursor-pointer mt-5 ml-0 sm:mx-auto text-white flex justify-center items-center"
+        >
+          Register
+        </div>
       </div>
-    </div>
-    <div class="lg:flex flex-row-reverse hidden">
-      <the-search-box
-        style="width: 560px"
-        class="lg:mt-4 xl:mt-16 xl:mr-8 -mt-32"
-      />
+      <div class="hero-right sm:w md:w-9/12 lg:w-6/12 w-7/12">
+        <div class="flex flex-row-reverse">
+          <img
+            class="md:w-8/12 lg:w-full"
+            src="../assets/images/hero-object.svg"
+            alt=""
+          />
+        </div>
+        <div class="lg:flex flex-row-reverse hidden">
+          <the-search-box
+            style="width: 560px"
+            class="lg:mt-4 xl:mt-16 xl:mr-8 -mt-32"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
