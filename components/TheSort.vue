73b8195385b96f791bf5fa10689d7d4f20743b07
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-28 19:44:59 +0330

    sort centralized

--- components/TheSort.vue
+++ components/TheSort.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="sort-container flex items-center justify-between mx-auto text-sm">
-      <div class="flex text-white cursor-pointer">
-          <img class="sm:mr-4 mr-1" src="../assets/images/sortburger.svg" alt="">
-          <span>BestSelling</span>
-      </div>
-      <div class="flex text-white cursor-pointer">
-          <img class="sm:mr-4 mr-1" src="../assets/images/Alphabetical.svg" alt="">
-          <span>Alphabetical</span>
-      </div>
-      <div class="flex text-white cursor-pointer">
-          <img class="sm:mr-4 mr-1" src="../assets/images/timesort.svg" alt="">
-          <span>Time</span>
-      </div>
+  <div
+    class="sort-container flex items-center justify-center sm:justify-between mx-auto text-sm"
+  >
+    <div class="flex text-white cursor-pointer">
+      <img class="sm:mr-4 mr-1" src="../assets/images/sortburger.svg" alt="" />
+      <span>BestSelling</span>
+    </div>
+    <div class="flex text-white cursor-pointer sm:mx-0 mx-5">
+      <img
+        class="sm:mr-4 mr-1"
+        src="../assets/images/Alphabetical.svg"
+        alt=""
+      />
+      <span>Alphabetical</span>
+    </div>
+    <div class="flex text-white cursor-pointer">
+      <img class="sm:mr-4 mr-1" src="../assets/images/timesort.svg" alt="" />
+      <span>Time</span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
-
